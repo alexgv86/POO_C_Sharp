a1f5cbf7ed70b93a36badfb3ada53acfeb731a3a
Carmen Mindeguia <carmen.mindeguia@squad.com.ar>
2021-09-18 13:37:15 -0300

    Agregar archivos de proyecto.

---
+++ PrimerApp/PrimerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PrimerBiblioteca;
+
+namespace PrimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimerClase pc2 = new PrimerClase ("Alejandro Saavedra", 35, 1.7 );
+            PrimerClase pc = new PrimerClase();
+            pc.NombreApellido = "Alejandro González Videla";
+            pc.Edad = 35;
+            pc.Altura = 1.89;
+
+            Console.WriteLine($"PC = {pc.NombreApellido} {pc.Edad} {pc.Altura}");
+            Console.WriteLine($"PC = {pc2.NombreApellido} {pc2.Edad} {pc2.Altura}");
+        }
+    }
+}
---
+++ PrimerApp/PrimerBiblioteca/PrimerClase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimerBiblioteca
+{
+    public class PrimerClase
+    {
+        private string _nombreApellido;
+        private int _edad;
+        private double _altura;
+        public PrimerClase() { }
+
+        public PrimerClase(string nombreApellido,int edad,double altura)
+        {
+            NombreApellido = nombreApellido;
+            _edad = edad/2;
+            _altura = altura;
+        }
+        public string NombreApellido { get; set; }
+
+        public double Altura
+        {
+            get { return _altura; }
+
+            set { _altura = value; }
+        }
+
+        public int Edad {
+            get { return _edad; }
+            set { _edad = value; } 
+        }
+
+    }
+}
