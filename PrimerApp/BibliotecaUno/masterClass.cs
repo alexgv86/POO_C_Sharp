27c8bbb4dcee736abab92358e3a43654a35f1eb1
alexgv86 <alexgv86@gmail.com>
2021-09-23 21:01:51 -0300

    Se agregó BibliotecaUno

---
+++ PrimerApp/BibliotecaUno/masterClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BibliotecaUno
+{
+    public class masterClass
+    {
+        private int _id;
+        private double _ancho;
+        private double _largo;
+        private double _superficie;
+        private string _nombre;
+
+        public masterClass()
+        {
+            _id = 0;
+            _ancho = _largo = _superficie = 0;
+            _nombre = string.Empty;
+        }
+
+        public masterClass(int id, double ancho, double largo, string nombre)
+        {
+            _id = id;
+            _ancho = ancho;
+            _largo = largo;
+            _superficie = ancho * largo;
+            _nombre = nombre;
+        }
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public double Ancho
+        {
+            get { return _ancho; }
+            set { _ancho = value; }
+        }
+        public double Largo
+        {
+            get { return _largo; }
+            set { _largo = value; }
+        }
+        public double Superficie
+        {
+            get { return _superficie; }
+        }
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public double CalcularSuperficie()
+        {
+            double resultado;
+            resultado = _largo * _ancho;
+            return resultado;
+        }
+    }
+}
--- PrimerApp/PrimerApp/Program.cs
+++ PrimerApp/PrimerApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using PrimerBiblioteca;
+using BibliotecaUno;
 
 namespace PrimerApp
 {
@@ -15,6 +16,20 @@ namespace PrimerApp
 
             Console.WriteLine($"PC = {pc.NombreApellido} {pc.Edad} {pc.Altura}");
             Console.WriteLine($"PC = {pc2.NombreApellido} {pc2.Edad} {pc2.Altura}");
+
+            masterClass mc = new masterClass();
+            masterClass mc1 = new masterClass(1, 2, 3, "Cuadrado");
+            Console.WriteLine(mc.Id);
+            Console.WriteLine(mc.Ancho);
+            Console.WriteLine(mc.Largo);
+            Console.WriteLine(mc.Superficie);
+            Console.WriteLine(mc.Nombre);
+
+            Console.WriteLine(mc1.Id);
+            Console.WriteLine(mc1.Ancho);
+            Console.WriteLine(mc1.Largo);
+            Console.WriteLine(mc1.Superficie);
+            Console.WriteLine(mc1.Nombre);
         }
     }
 }
